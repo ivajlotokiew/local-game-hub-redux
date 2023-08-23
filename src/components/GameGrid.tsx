fcd4723a491b4ef47d79483352b0020c30c52f8f
ivajlotokiew <ivajlo_nick@abv.bg>
2023-08-23 11:19:30 +0300

    error handling on gameSlice

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,14 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { Button, HStack, SimpleGrid, Text } from "@chakra-ui/react";
-import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 import { GameQuery } from "../App";
 import { useDispatch, useSelector } from "react-redux";
-import { selectGames } from "../features/games/gameSlice";
+import { selectGames, selectErrorState, selectLoadingState } from "../features/games/gameSlice";
 import { getGames } from "../features/games/gameSlice";
-
 
 interface Props {
   gameQuery: GameQuery | null
@@ -19,27 +17,26 @@ const GAMES_PER_PAGE = 20
 const GameGrid = ({ gameQuery }: Props) => {
   const [page, setPage] = useState(0)
   const [endpoint, setEndpoint] = useState('/games')
-  const { data: games, error, isLoading, count } = useGames(endpoint, gameQuery)
-  const dispatch = useDispatch()
+  const dispatch = useDispatch<any>()
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+  const games = useSelector(selectGames);
+  const err = useSelector(selectErrorState);
+  const isLoading = useSelector(selectLoadingState);
+  const count = useSelector(state => state.count)
   const allPages = Math.ceil(count / GAMES_PER_PAGE)
   const nextBtnDisabled = page === allPages || page === 0 && allPages === 1 || Number.isNaN(allPages) ? true : false
-  const gms = useSelector(selectGames);
-  console.log('Games: ', gms)
 
   useEffect(() => {
-    debugger
     dispatch(getGames())
-
   }, [])
 
   useEffect(() => {
     setPage(0)
   }, [gameQuery?.genre])
 
-  useEffect(() => {
-    page > 0 ? setEndpoint('/games?page=' + page) : setEndpoint('/games')
-  }, [page])
+  // useEffect(() => {
+  //   page > 0 ? setEndpoint('/games?page=' + page) : setEndpoint('/games')
+  // }, [page])
 
   const setPrevPage = () => {
     setPage(oldPage => oldPage === 2 ? 0 : oldPage - 1)
@@ -51,13 +48,13 @@ const GameGrid = ({ gameQuery }: Props) => {
 
   return (
     <>
-      {error && <Text>{error}</Text>}
+      {err && <Text>{err.description}</Text>}
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} p='10px' spacing={6}>
         {isLoading && skeletons.map(skeleton =>
           <GameCardContainer key={skeleton}>
             <GameCardSkeleton />
           </GameCardContainer>)}
-        {gms.map(game =>
+        {games?.map(game =>
           <GameCardContainer key={game.id} >
             <GameCard game={game} />
           </GameCardContainer>
---
+++ src/features/games/gameSlice.tsx
@@ -0,0 +1,69 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import apiClient from "../../services/api-client";
+import { Game } from "../../hooks/useGames";
+
+interface ServerError {
+  statusCode: number
+  description: string
+}
+
+interface gameState {
+  games: Game[],
+  isLoading: boolean,
+  error: ServerError,
+  count: number,
+}
+
+type RootState = {
+  gameList: gameState
+}
+
+const gameSlice = createSlice({
+  name: "gameList",
+  initialState: {
+    games: [],
+    isLoading: false,
+    error: {} as ServerError,
+    count: 0,
+  } as gameState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getGames.pending, (state) => {
+        debugger
+        state.isLoading = true;
+        state.error = {} as ServerError;
+      })
+      .addCase(getGames.fulfilled, (state, action) => {
+        debugger
+        state.games = action.payload.results;
+        state.count = action.payload.count;
+        state.isLoading = false;
+        state.error = {} as ServerError;
+      })
+      .addCase(getGames.rejected, (state, { payload }) => {
+        debugger
+        const msg = payload.message
+        const status = payload.response.status
+        state.error = { statusCode: Number(status), description: msg };
+        state.isLoading = false;
+      });
+  },
+});
+
+export const getGames = createAsyncThunk('data/update', async (input, { rejectWithValue }) => {
+  try {
+    debugger
+    const response = await apiClient.get("/games");
+    return response.data;
+  } catch (error) {
+    return rejectWithValue(error);
+  }
+});
+
+// Selectors
+export const selectGames = (state: RootState) => state.gameList.games;
+export const selectLoadingState = (state: RootState) => state.gameList.isLoading;
+export const selectErrorState = (state: RootState) => state.gameList.error;
+
+export default gameSlice.reducer;
