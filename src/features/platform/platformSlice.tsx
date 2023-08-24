e429f7597ff802555d5e4bbcc4ba1f717711446b
ivajlotokiew <ivajlo_nick@abv.bg>
2023-08-24 10:50:20 +0300

    Create platformSlice

---
+++ src/features/platform/platformSlice.tsx
@@ -0,0 +1,68 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import apiClient from "../../services/api-client";
+import { Platform } from "../../hooks/usePlatforms";
+
+interface ServerError {
+  statusCode: number;
+  description: string;
+}
+
+interface platformState {
+  platforms: Platform[];
+  isLoading: boolean;
+  error: ServerError;
+}
+
+type RootState = {
+  platformList: platformState;
+};
+
+const platformSlice = createSlice({
+  name: "platformList",
+  initialState: {
+    platforms: [],
+    isLoading: false,
+    error: {} as ServerError,
+  } as platformState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getPlatforms.pending, (state) => {
+        state.isLoading = true;
+        state.error = {} as ServerError;
+      })
+      .addCase(getPlatforms.fulfilled, (state, { payload }) => {
+        state.platforms = payload.results;
+        state.isLoading = false;
+        state.error = {} as ServerError;
+      })
+      .addCase(getPlatforms.rejected, (state, { payload }) => {
+        const msg = payload.message;
+        const status = payload.response.status;
+        state.error = { statusCode: Number(status), description: msg };
+        state.isLoading = false;
+      });
+  },
+});
+
+export const getPlatforms = createAsyncThunk(
+  "platformList/getPlatforms",
+  async (endpoint: string, { rejectWithValue }) => {
+    debugger;
+    try {
+      const response = await apiClient.get(`${endpoint}`);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
+
+// Selectors
+export const selectPlatforms = (state: RootState) =>
+  state.platformList.platforms;
+export const selectLoadingState = (state: RootState) =>
+  state.platformList.isLoading;
+export const selectErrorState = (state: RootState) => state.platformList.error;
+
+export default platformSlice.reducer;
--- src/store.js
+++ src/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import gameReducer from "./features/game/gameSlice";
 import genreReducer from "./features/genre/genreSlice";
+import platformReducer from "./features/platform/platformSlice";
 
 const store = configureStore({
   reducer: {
     gameList: gameReducer,
     genreList: genreReducer,
+    platformList: platformReducer,
   },
 });
 
